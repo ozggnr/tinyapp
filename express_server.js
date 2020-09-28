c40fcb65e9c3fc9f376b8d1d317c8bf6401afcc0
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-28 22:34:06 +0000

    adding second route

--- express_server.js
+++ express_server.js
@@ -5,34 +5,33 @@ const PORT = 8080; //default port 8080
 app.set("view engine", "ejs");
 
 
-
-
-
-
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls", (reg,res) => {
+  const templateVars = {urls : urlDatabase};
+  res.render("urls_index", templateVars);
+})
 
 app.get("/", (req,res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = {greeting : 'hello world'};
+  res.render("hello_world", templateVars)
 });
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL : req.params.shortURL,
+    longURL : urlDatabase[req.params.shortURL]
+  }
+  res.render("urls_show",templateVars)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
