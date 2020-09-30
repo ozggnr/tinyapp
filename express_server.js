aa33ddd3ef372d3845f44cc7f59b0ec72b0a044a
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-30 04:14:48 +0000

    cookies added

--- express_server.js
+++ express_server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = 8080; //default port 8080
 //set ejs as the view engine
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 //define the database which we will use or add different values
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 //render database in urls_index
-app.get("/urls", (reg,res) => {
-  const templateVars = {urls : urlDatabase};
+app.get("/urls", (req,res) => {
+  const templateVars = {username: req.cookies["username"],urls : urlDatabase};
   res.render("urls_index", templateVars);
 })
 //create random string for shortURL
@@ -23,17 +25,30 @@ app.post("/urls", (req, res) => {
   // console.log(urlDatabase); 
   res.redirect(`/urls/${randomString}`);
 });
-
 app.get("/urls/new", (req,res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new",templateVars);
 })
 
+app.post("/login", (req,res) => {
+  res.cookie("username",req.body.username)
+  // console.log(req)
+  res.redirect("/urls")
+})
+app.get('/login', (req,res) => {
+  const templateVars = {username: req.body.username}
+  res.render("urls",templateVars)
+})
+app.post('/logout', (req,res) => {
+  res.clearCookie("username")
+  res.redirect("/urls")
+})
 app.get("/urls/:shortURL", (req, res) => {
   console.log(req.params.shortURL)
   const templateVars = {
+  username: req.cookies["username"],
   longURL : urlDatabase[req.params.shortURL],
   shortURL : req.params.shortURL}
-  // console.log(req.params)
   res.render("urls_show", templateVars)
 });
 //for our database
